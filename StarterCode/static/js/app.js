1224f8fbc729b04a5d265018655933d73b916300
StephanieMcNutt <stephie4321@gmail.com>
2021-06-24 10:32:08 -0400

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -35,3 +35,42 @@ function metadata(sampleid){
   }
 
 
+//Setup Dropdown menu click
+function optionChanged(newData){
+    metadata(newData);
+    buildChart(newData);
+  }
+
+//Building Chart function
+function buildChart(sampleid){
+    d3.json("samples.json").then((sampledata)=>{
+        var samples = sampledata.samples;
+        var dataArray = samples.filter(row=>row.id==sampleid);
+        console.log(dataArray);
+        var sample = dataArray[0];
+        var otu_ids = sample.otu_ids;
+        var sample_values = sample.sample_values;
+        var otu_labels = sample.otu_labels;
+        var bubbledata = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }];
+        Plotly.newPlot("bubble", bubbledata);
+        var bardata = [{
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_ids.slice(0,10).map(otu_ids=>`OTU${otu_ids}`).reverse(),
+            text: otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }];
+        Plotly.newPlot("bar", bardata);
+  
+    })
+  }
